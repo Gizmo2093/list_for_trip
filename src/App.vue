d8f681731d8384a91d4a9de8f56539f982e23571
Gizmo2093 <gizmo2093@gmail.com>
2021-10-02 18:00:14 +0700

    Create

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app>
+      <v-toolbar-title>Brand</v-toolbar-title>
+
+      <v-spacer />
+
+      <v-btn icon x-large @click="editProfile">
+        <v-avatar>
+          <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
+        </v-avatar>
+      </v-btn>
+
+    </v-app-bar>
+
+    <main>
+      <Home />
+    </main>
+
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import Home from './views/Home.vue'
+export default {
+  name: 'App',
+  data: () => ({
+    user: {
+      initials: 'VE',
+      fullName: 'Vladimir',
+      email: '123@gmail.com',
+    }
+  }),
+  components:{
+    Home
+  },
+  methods:{
+    editProfile(){
+      console.log("Перенаправление на страницу редактирования профиля");
     }
   }
-}
-</style>
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-main class="">
+    <v-toolbar dark color="teal">
+      <v-autocomplete cache-items class="mx-4" flat hide-no-data hide-details label="Куда отправимся сегодня?" solo-inverted/>
+      </v-toolbar>
+
+    <v-container>
+      <v-row>
+        <v-col
+          v-for="n in 24"
+          :key="n"
+          cols="4"
+        >
+          <v-card height="200">
+            {{ countries }}
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+const axios = require('axios').default;
+  export default {
+    name: 'Home',
+    data: () =>({
+      msg: 'Home page',
+      countries: null,
+    }),
+    components: {
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+    },
+    mounted(){
+      axios
+        .get('http://testslim/countries/all',{
+          headers: { 
+            'Access-Control-Allow-Origin' : '*',
+          },
+        })
+        .then(response => (this.countries = response))
+    }
   }
-}
 </script>
+
+<style scoped>
+#Home{
+  margin-top: 150px;
+}
+</style>
